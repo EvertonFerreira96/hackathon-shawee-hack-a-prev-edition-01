6ea171fd8ba8f59f2b0219d739c676a606a60750
Everton Ferreira <ferreira.everton@icloud.com>
2020-06-01 00:04:55 -0300

    Hack A Prev

--- mobile/src/pages/Chat/index.js
+++ mobile/src/pages/Chat/index.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Constants from 'expo-constants';
 import { WebView } from 'react-native-webview'
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { Entypo, MaterialCommunityIcons,AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-function Chat({ navigation }) {
+function Chat() {
+    const navigation = useNavigation();
     return (
         <>
-            <View style={{paddingTop: Constants.statusBarHeight + 0, backgroundColor:'#FFF'}}>
+            <TouchableOpacity onPress={() => {navigation.goBack()}} style={{paddingTop: Constants.statusBarHeight + 0, backgroundColor:'#FFF'}}>
             <AntDesign name="arrowleft" size={24} color="#051A77" style={{padding:15}} />
-            </View>
+            </TouchableOpacity>
             <WebView 
                 style={{ flex: 1}} source={{ uri: `https://app.cosmobots.io/landing/541bb950-a347-11ea-8104-5ff706eadbf3` }}/>
 
