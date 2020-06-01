bc2fd342aa2030d59d2b8cc6064ecec352da43f8
Everton Ferreira <ferreira.everton@icloud.com>
2020-06-01 00:03:04 -0300

    Hack A Prev

--- mobile/src/pages/Chat/index.js
+++ mobile/src/pages/Chat/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Constants from 'expo-constants';
 import { WebView } from 'react-native-webview'
+import { View } from 'react-native';
+import { Entypo, MaterialCommunityIcons,AntDesign } from '@expo/vector-icons';
 
 function Chat({ navigation }) {
-    return <WebView style={{ flex: 1}} source={{ uri: `https://app.cosmobots.io/landing/541bb950-a347-11ea-8104-5ff706eadbf3` }}/>
-}
+    return (
+        <>
+            <View style={{paddingTop: Constants.statusBarHeight + 0, backgroundColor:'#FFF'}}>
+            <AntDesign name="arrowleft" size={24} color="#051A77" style={{padding:15}} />
+            </View>
+            <WebView 
+                style={{ flex: 1}} source={{ uri: `https://app.cosmobots.io/landing/541bb950-a347-11ea-8104-5ff706eadbf3` }}/>
+
+        </>
+    )
+   }
 
 export default Chat
